a50a45e8a6f5fc12e2a8046bdd326244e8398871
flonix8 <flstanek@googlemail.com>
2025-10-28 14:23:36 +0100

    Add solution 02.03

--- 02-language-introduction/03-data/src/main/java/de/starwit/Data.java
+++ 02-language-introduction/03-data/src/main/java/de/starwit/Data.java
@@ -1,72 +1,105 @@
 package de.starwit;
 
+import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.Random;
 
 public class Data {
     static Logger log = LogManager.getLogger(Data.class.getName());
 
     public static void main(String[] args) throws Exception {
-        log.info("program started");
-        dataTypes();
-        moreDataTypes();
-        arrays();
+        log.info("Please enter some integers:");
+
+        String inputLine = readLine();
+        String[] lineItems = splitLine(inputLine);
+
+        if (!isAllInt(lineItems)) {
+            log.warn("Input contained non-integer");
+            return;
+        };
+
+        int[] intItems = convertToIntArray(lineItems);
+
+        double[] output = calculateOutput(intItems);
+
+        log.info("Minimum is: " + output[0]);
+        log.info("Maximum is: " + output[1]);
+        log.info("Sum is: " + output[2]);
+        log.info("Average is: " + output[3]);
     }
 
-    public static void dataTypes() {
-        String example = "String example";
-        int aNumber = new Random().nextInt(0, 100 + 1);
-        double aFloatingNumber = 1.5d;
-        boolean isTrue = false;
-
-        log.info("putting it all together: " + example + " " + aNumber + " " + aFloatingNumber + " " + isTrue);
+    public static String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
     }
 
-    public static void moreDataTypes() {
-        log.info("more data types");
-        long bigNumber = 654234235;
-        short smallNumber = 500;
-        byte evenSmallerNumber = 127;
-
-        char singleCharacter = 'a';
+    public static String[] splitLine(String line) {
+        return line.split(" ");
     }
 
-    public static void strings() {
-        log.info("working with strings");
-        String example = "This is an example string";
-
-        log.info("Length of string: " + example.length());
-        log.info("Character at position 5: " + example.charAt(5));
-        log.info("Uppercase: " + example.toUpperCase());
-        log.info("Lowercase: " + example.toLowerCase());
-        log.info("Contains 'example': " + example.contains("example"));
-        log.info("Replace 'example' with 'sample': " + example.replace("example", "sample"));
-    }
-
-    public static void arrays() {
-        log.info("Working with arrays");
-
-        // defining an array
-        log.info("Creating a string array");
-        String[] stringArray = new String[10];
-        log.info("Filling array with values using classic for loop");
-        for(int i = 0; i < 10; i++) {
-            stringArray[i] = "string" + i;
+    public static boolean isAllInt(String[] lineItems) {
+        for (String item : lineItems) {
+            try {
+                Integer.parseInt(item);
+            } catch (NumberFormatException e) {
+                return false;
+            }
         }
 
-        // output array
-        log.info("Outputting array content using for-each loop");
-        for (String s : stringArray) {
-            log.info(s);
-        }
-
-        //access random element
-        log.info("Accessing element at index 5");
-        log.info(stringArray[5]);
-
-        //how many items in array?
-        log.info("Number of items in array: " + stringArray.length);
+        return true;
     }
+
+    public static int[] convertToIntArray(String[] lineItems) {
+        int[] intItems = new int[lineItems.length];
+        for (int i = 0; i < lineItems.length; i++) {
+            intItems[i] = Integer.parseInt(lineItems[i]);
+        }
+        return intItems;
+    }
+
+    public static double[] calculateOutput(int[] intItems) {
+        // minimum, maximum, sum, average
+        double[] statValues = new double[4];
+        
+        statValues[0] = calculateMinimum(intItems);
+        statValues[1] = calculateMaximum(intItems);
+        statValues[2] = calculateSum(intItems);
+        statValues[3] = calculateAverage(intItems);
+
+        return statValues;
+    }
+
+    public static double calculateMinimum(int[] arr) {
+        int minimum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < minimum) {
+                minimum = arr[i];
+            }
+        }
+        return minimum;
+    }
+
+    public static double calculateMaximum(int[] arr) {
+        int maximum = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maximum) {
+                maximum = arr[i];
+            }
+        }
+        return maximum;
+    }
+
+    public static int calculateSum(int[] arr) {
+        int sum = 0;
+        for (int item : arr) {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static double calculateAverage(int[] arr) {
+        return calculateSum(arr) / arr.length;
+    }
+
 }
