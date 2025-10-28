05ee2df10175ee1ca9a369ca302327e7daf9e466
flonix8 <flstanek@googlemail.com>
2025-10-28 15:07:07 +0100

    Add tests

--- 02-language-introduction/03-data/src/test/java/de/starwit/DataTest.java
+++ 02-language-introduction/03-data/src/test/java/de/starwit/DataTest.java
@@ -9,4 +9,29 @@ public class DataTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    @Test
+    public void testCalculateMinimum() {
+        double minimum = Data.calculateMinimum(new int[] {3, -1, 2, 100, -20});
+        assertTrue(minimum == -20);
+    }
+
+    @Test
+    public void testCalculateMaximum() {
+        double minimum = Data.calculateMaximum(new int[] {3, -1, 2, 100, -20});
+        assertTrue(minimum == 100);
+    }
+
+    @Test
+    public void testCalculateSum() {
+        double minimum = Data.calculateSum(new int[] {3, -1, 2, 100, -20});
+        assertTrue(minimum == 84);
+    }
+
+    @Test
+    public void testCalculateAverage() {
+        double minimum = Data.calculateAverage(new int[] {10, 5, 50, 100, 20});
+        assertTrue(minimum == 185. / 5);
+    }
+    
 }
